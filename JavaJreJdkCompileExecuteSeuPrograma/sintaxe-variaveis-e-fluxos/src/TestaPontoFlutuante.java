c083955af02384cfecba70088f7e98c41a5072d6
Jossany Moura <mjossany@gmail.com>
2022-11-23 20:08:59 -0300

    Trabalhando com pontos flutuantes

---
+++ JavaJreJdkCompileExecuteSeuPrograma/sintaxe-variaveis-e-fluxos/src/TestaPontoFlutuante.java
@@ -0,0 +1,21 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 5300.50;
+		System.out.println("Meu salário será: " + salario);
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novaDivisao = 5 / 2;
+		System.out.println(novaDivisao);
+		
+		double novissimaDivisao = 5.0 / 2;
+		System.out.println(novissimaDivisao);
+	}
+}
