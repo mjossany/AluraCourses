a8c89cbc169ccd34aa4a3c259e5838a5718ff180
Jossany Moura <mjossany@gmail.com>
2022-11-26 10:01:13 -0300

    aprendendo sobre métodos, uso do this e retorno de métodos

--- JavaOO/bytebank/src/bytebank/Conta.java
+++ JavaOO/bytebank/src/bytebank/Conta.java
@@ -7,4 +7,26 @@ public class Conta {
 	int numero;
 	String titular;
 	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			saca(valor);
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
