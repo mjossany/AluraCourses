8b6480b1628d401f26eda7b05f683f142e68093f
Jossany Moura <mjossany@gmail.com>
2022-11-27 22:23:56 -0300

    trabalhando com composição de objetos e referência null

---
+++ JavaOO/bytebank-composto/src/Teste.java
@@ -0,0 +1,12 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Conta contaDoCliente = new Conta();
+		contaDoCliente.titular = new Cliente();
+
+		contaDoCliente.titular.nome = "Jossany";
+
+	}
+}
