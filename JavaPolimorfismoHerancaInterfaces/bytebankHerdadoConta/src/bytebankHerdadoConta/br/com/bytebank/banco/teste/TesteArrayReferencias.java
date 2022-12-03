096bdc8a56f450482e1b93356157499f8e1e8599
Jossany Moura <mjossany@gmail.com>
2022-12-03 18:55:13 -0300

    aprendendo e trabalhando com ArrayList e Generics

--- JavaPolimorfismoHerancaInterfaces/bytebankHerdadoConta/src/bytebankHerdadoConta/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ JavaPolimorfismoHerancaInterfaces/bytebankHerdadoConta/src/bytebankHerdadoConta/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -7,18 +7,17 @@ import bytebankHerdadoConta.br.com.bytebank.banco.modelo.ContaPoupanca;
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
-		
+
 		Conta[] contas = new Conta[5];
-		
+
 		ContaCorrente cc1 = new ContaCorrente(4331, 12345);
 		ContaPoupanca cc2 = new ContaPoupanca(4331, 12346);
-		
+
 		contas[0] = cc1;
 		contas[1] = cc2;
-		
+
 		ContaPoupanca ref = (ContaPoupanca) contas[1];
-		
-		
+
 		System.out.println(contas[0].getNumero());
 
 	}
---
+++ JavaPolimorfismoHerancaInterfaces/bytebankHerdadoConta/src/bytebankHerdadoConta/br/com/bytebank/banco/teste/util/Teste.java
@@ -0,0 +1,40 @@
+package bytebankHerdadoConta.br.com.bytebank.banco.teste.util;
+
+import java.util.ArrayList;
+
+import bytebankHerdadoConta.br.com.bytebank.banco.modelo.Conta;
+import bytebankHerdadoConta.br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		ArrayList<Conta> lista = new ArrayList<Conta>();
+
+		Conta cc = new ContaCorrente(11, 111);
+		lista.add(cc);
+
+		Conta cc2 = new ContaCorrente(22, 222);
+		lista.add(cc2);
+
+		System.out.println("Tamanho: " + lista.size());
+
+		Conta ref = lista.get(0);
+		System.out.println(ref.getNumero());
+
+		lista.remove(0);
+		System.out.println("Tamanho: " + lista.size());
+
+		Conta cc3 = new ContaCorrente(33, 333);
+		lista.add(cc3);
+
+		Conta cc4 = new ContaCorrente(44, 444);
+		lista.add(cc4);
+
+		for (Object oRef : lista) {
+			System.out.println(oRef);
+		}
+
+	}
+
+}
