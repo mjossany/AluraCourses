f65622a855299c8691a122c05d6ffd4ade2468bd
Jossany Moura <mjossany@gmail.com>
2022-12-03 17:23:25 -0300

    aprendendo e trabalhando com referências em arrays de objetos

--- JavaPolimorfismoHerancaInterfaces/bytebankHerdadoConta/src/bytebankHerdadoConta/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ JavaPolimorfismoHerancaInterfaces/bytebankHerdadoConta/src/bytebankHerdadoConta/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,18 +1,23 @@
 package bytebankHerdadoConta.br.com.bytebank.banco.teste;
 
+import bytebankHerdadoConta.br.com.bytebank.banco.modelo.Conta;
 import bytebankHerdadoConta.br.com.bytebank.banco.modelo.ContaCorrente;
+import bytebankHerdadoConta.br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Conta[] contas = new Conta[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(4331, 12345);
-		ContaCorrente cc2 = new ContaCorrente(4331, 12346);
+		ContaPoupanca cc2 = new ContaPoupanca(4331, 12346);
 		
 		contas[0] = cc1;
 		contas[1] = cc2;
+		
+		ContaPoupanca ref = (ContaPoupanca) contas[1];
+		
 		
 		System.out.println(contas[0].getNumero());
 
